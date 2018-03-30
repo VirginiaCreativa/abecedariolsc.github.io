106436ebb82835dc67a34b5e78fc89fe81520c35
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-03-30 13:09:38 -0500

    Decoration index and app.js from try

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,40 @@
-console.log('HELLO WORLD');
+window.addEventListener('keydown', handleKey)
+
+function handleKey (ev) {
+	console.log(ev.keyCode);
+	const title = document.querySelector('.keyAbece__title')
+	title.style.display = 'none'
+	const number = document.querySelector('.keyAbece__hand')
+
+	number.innerHTML = keyCodes[ev.keyCode] ? ev.keyCode : 'Solo alfabero';
+	// if(keyCodes[ev.keyCode]) {
+}
+const keyCodes = {
+  65 : "a",
+  66 : "b",
+  67 : "c",
+  68 : "d",
+  69 : "e",
+  70 : "f",
+  71 : "g",
+  72 : "h",
+  73 : "i",
+  74 : "j",
+  75 : "k",
+  76 : "l",
+  77 : "m",
+  78 : "n",
+  79 : "o",
+  80 : "p",
+  81 : "q",
+  82 : "r",
+  83 : "s",
+  84 : "t",
+  85 : "u",
+  86 : "v",
+  87 : "w",
+  88 : "x",
+  89 : "y",
+  90 : "z",
+
+};
