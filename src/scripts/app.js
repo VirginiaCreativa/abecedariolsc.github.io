1e6d4aa454544378a880877ebeece3b381d5e21f
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-04-13 18:01:20 -0500

    Funciona HAND

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,8 @@
 
-let letter = document.querySelector('.keyCodMostrar__letter')
+let showLetter = document.querySelector('.keyCodMostrar__letter')
 let title = document.querySelector('.keyAbece__title')
 let hand = document.querySelector('.keyAbece__hand')
+let handIcon = document.querySelector('.iconHand')
 let error = document.querySelector('.keyAbece__error')
 
 const keyCodes = {
@@ -32,6 +33,34 @@ const keyCodes = {
   89 : "y",
   90 : "z",
 };
+const keyHands = {
+  65 : "images/abecedario_Aa.svg",
+  66 : "images/abecedario_Bb.svg",
+  67 : "images/abecedario_Cc.svg"
+  // 68 : "images/abecedario_Dd.svg",
+  // 69 : "images/abecedario_Ee.svg",
+  // 70 : "images/abecedario_Ff.svg",
+  // 71 : "images/abecedario_Gg.svg",
+  // 72 : "images/abecedario_Hh.svg",
+  // 73 : "images/abecedario_Ii.svg",
+  // 74 : "images/abecedario_Jj.svg",
+  // 75 : "images/abecedario_Kk.svg",
+  // 76 : "images/abecedario_Ll.svg",
+  // 77 : "images/abecedario_Mm.svg",
+  // 78 : "images/abecedario_Nn.svg",
+  // 79 : "images/abecedario_Ññ.svg",
+  // 80 : "images/abecedario_Oo.svg",
+  // 81 : "images/abecedario_Pp.svg",
+  // 82 : "images/abecedario_Qq.svg",
+  // 83 : "images/abecedario_Rr.svg",
+  // 84 : "images/abecedario_Ss.svg",
+  // 85 : "images/abecedario_Tt.svg",
+  // 86 : "images/abecedario_Uu.svg",
+  // 87 : "images/abecedario_Vv.svg",
+  // 88 : "images/abecedario_Ww.svg",
+  // 89 : "images/abecedario_Xx.svg",
+  // 90 : "images/abecedario_Yy.svg",
+};
 
 function handleKeyDown (ev) {
   if ( !ev.metaKey ) {
@@ -40,26 +69,32 @@ function handleKeyDown (ev) {
   
   title.style.display = 'none'
   error.style.display = 'none'
-
   
-
   let key = (ev.keyCode) ? ev.keyCode : ev.which;
 
+  const imgBloq = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP94z19hOeXX6UD_qr9cabEAnh9OEYRo1zSMBItiRu42i3od71'
+
   if (keyCodes[ev.keyCode] && key) {
-    console.log(ev.keyCode);
-    letter.innerHTML = keyCodes[ev.keyCode]
-    letter.classList.add('active')
+
+    const letterKC = keyCodes[ev.keyCode]
+    const handKC = keyHands[ev.keyCode]
+
+    showLetter.innerHTML = letterKC
+    handIcon.src = handKC ? handKC : imgBloq;
+    hand.style.display = 'block'
+
+    console.log(handKC);
+
+    showLetter.classList.add('active')
   } else {
     error.style.display = 'block'
+    handIcon.src = ''
   }
-
-
 
 }
 
 function handleKeyUp (ev) {
- letter.classList.remove('active')
-
+  showLetter.classList.remove('active')
 }
 
 window.addEventListener('keyup', handleKeyUp);
